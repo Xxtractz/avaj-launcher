ff59ce602c725c1d552c2fa20dbb5481ebcab588
Xxtractz <musambaloyi@gmail.com>
2019-07-08 15:30:23 +0200

    AircraftFactory

--- src/Aircraft/AircraftFactory.java
+++ src/Aircraft/AircraftFactory.java
@@ -1,4 +1,8 @@
 package Aircraft;
 
-public abstract class AircraftFactory {
+public abstract class AircraftFactory
+{
+    public static void /*Flyable*/ newAircraft(String type, String name, int Longitude, int latitude, int height)
+    {
+    }
 }
