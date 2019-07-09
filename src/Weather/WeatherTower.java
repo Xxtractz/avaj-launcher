3272575d11449abb1f9ad1cf7363c915e13006fc
Musa Baloyi <musambaloyi@gmail.com>
2019-07-09 23:10:12 +0200

    Weather

--- src/Weather/WeatherTower.java
+++ src/Weather/WeatherTower.java
@@ -2,9 +2,12 @@ package Weather;
 
 import Aircraft.Coordinates;
 
-public class WeatherTower extends Tower{
-    public String getWeather(Coordinates coordinates){
-        return "";
+public class WeatherTower extends Tower
+{
+    public String getWeather(Coordinates coordinates)
+    {
+        String weather = WeatherProvider.getProvider().getCurrentWeather(coordinates);
+        return weather;
     }
 
     void changeWeather(){
