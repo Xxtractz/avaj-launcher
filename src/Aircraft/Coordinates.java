58fe31e56c14c402d58665aab1fca55e36e5d4cb
Xxtractz <musambaloyi@gmail.com>
2019-07-10 16:45:23 +0200

    Hellicopter

--- src/Aircraft/Coordinates.java
+++ src/Aircraft/Coordinates.java
@@ -5,8 +5,21 @@ public class Coordinates {
     private int latitude;
     private int height;
 
+
     Coordinates(int longitude, int latitude, int height ){
 
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public int getLongitude() {
