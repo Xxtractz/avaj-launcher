f544a8784aca14219a002f384adc190c94acd70e
Xxtractz <musambaloyi@gmail.com>
2019-07-10 16:22:34 +0200

    Tower

--- src/Weather/Tower.java
+++ src/Weather/Tower.java
@@ -8,14 +8,19 @@ public abstract class Tower {
     private ArrayList<Flyable> observers = new ArrayList<Flyable>();
 
     public void register(Flyable flyable){
+        observers.add(flyable);
 
     }
 
     public void unregister(Flyable flyable){
+        observers.remove(flyable);
 
     }
 
     protected void conditionsChanged(){
+        for (int i = 0; i < observers.size(); i++) {
+            observers.get(i).updateConditions();
+        }
 
     }
 }
