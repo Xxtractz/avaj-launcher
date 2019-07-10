e1a007295c31ebed586bc9b09aebe9c15548d205
Xxtractz <musambaloyi@gmail.com>
2019-07-10 16:28:20 +0200

    Weather Complete

--- src/Weather/Tower.java
+++ src/Weather/Tower.java
@@ -9,18 +9,15 @@ public abstract class Tower {
 
     public void register(Flyable flyable){
         observers.add(flyable);
-
     }
 
     public void unregister(Flyable flyable){
         observers.remove(flyable);
-
     }
 
     protected void conditionsChanged() throws Exception{
         for (int i = 0; i < observers.size(); i++) {
             observers.get(i).updateConditions();
         }
-
     }
 }
