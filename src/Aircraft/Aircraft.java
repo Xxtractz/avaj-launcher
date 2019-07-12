653aa74069b503b3ac5f1ecff46d8b9395e83486
xxtractz <musambaloyi@gmail.com>
2019-07-12 13:08:02 +0200

    update aircraft

--- src/Aircraft/Aircraft.java
+++ src/Aircraft/Aircraft.java
@@ -4,13 +4,15 @@ public abstract class Aircraft {
     protected Long id;
     protected String name;
     protected Coordinates coordinates;
-    private static Long idCounter;
+    private static Long idCounter  = 0L;
 
     protected Aircraft(String name ,Coordinates coordinates){
-
+        this.id = nextId();
+        this.coordinates = coordinates;
+        this.name= name;
     }
 
-    private long nextId(){
-        return 0;
+    private Long nextId(){
+        return ++this.idCounter;
     }
 }
