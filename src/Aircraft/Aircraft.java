b3a226f0448ca112d6b28e41448a7b61a7de4b9d
xxtractz <musambaloyi@gmail.com>
2019-07-15 21:11:18 +0200

    update Aircraft

--- src/Aircraft/Aircraft.java
+++ src/Aircraft/Aircraft.java
@@ -1,10 +1,13 @@
 package Aircraft;
 
+import Simulator.Logger;
+
 public abstract class Aircraft {
     protected Long id;
     protected String name;
     protected Coordinates coordinates;
     private static Long idCounter  = 0L;
+    protected Logger logger = new Logger();
 
     protected Aircraft(String name ,Coordinates coordinates){
         this.id = nextId();
